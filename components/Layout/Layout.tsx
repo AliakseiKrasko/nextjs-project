0d28c34fe388f06b29c15e34c194bc34e142c203
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-09 10:20:30 +0300

    added styles to Navbar

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+// @flow 
+import * as React from 'react';
+
+type Props = {
+    
+};
+export const Layout = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import styles from './Navbar.module.scss'
 
 export const Navbar = () => {
     return (
-        <div>
+        <div className={styles.links}>
             <Link href={'/'}>Main</Link>
             <Link href={'/characters'}>Characters</Link>
         </div>
