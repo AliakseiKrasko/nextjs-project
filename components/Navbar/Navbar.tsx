6ded48f46fb5386f5529ff45182884b4955599f6
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-09 10:10:04 +0300

    create Navbar and Link Main, Characters

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export const Navbar = () => {
+    return (
+        <div>
+            <Link href={'/'}>Main</Link>
+            <Link href={'/characters'}>Characters</Link>
+        </div>
+    );
+};
